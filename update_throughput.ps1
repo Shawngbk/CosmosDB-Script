090c63115a90e18a72f9c0b9a4e0693a72f852ae
Shawngbk <junbo0711@outlook.com>
2020-05-07 11:18:39 +0800

    Create update_throughput.ps1

---
+++ update_throughput.ps1
@@ -0,0 +1,17 @@
+﻿$apiVersion = "2016-03-31"
+$resourceGroupName = "shawngbk"
+$accountName = "tableapi"
+$tableName = "test"
+$tableThroughputResourceName = $accountName + "/table/" + $tableName + "/throughput"
+$tableThroughputResourceType = "Microsoft.DocumentDb/databaseAccounts/apis/tables/settings"
+$throughput = 500
+$tableProperties = @{
+    "resource"=@{"throughput"=$throughput}
+} 
+$PSDefaultParameterValues["Disabled"]=$true
+
+Set-AzResource `
+    -ResourceType $tableThroughputResourceType -ApiVersion $apiVersion `
+    -ResourceGroupName $resourceGroupName -Name $tableThroughputResourceName `
+    -Properties $tableProperties `
+    -Force
